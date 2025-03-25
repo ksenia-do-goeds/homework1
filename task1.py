cc88bcbe9283e32acd1c899f7db18346437451ec
ksenia-do-goeds <k5ushkaa@yandex.ru>
2025-03-25 14:32:41 +0300

    task1.py

--- task1.py
+++ task1.py
@@ -4,8 +4,10 @@ age = input('Ваш возраст: ')
 
 # Реализация через format
 formatted_string = "Ваше имя: {}, Фамилия: {}, Возраст: {} лет.".format(name, surname, age)
-print(formatteg_string)
+print("Реализация через format:")
+print(formatted_string)
 
 # Реализация через f-string
-f-string = f"Ваше имя: {name}, Фамилия: {surname}, Возраст: {age} лет."
-print(f-string)
+f_string = f"Ваше имя: {name}, Фамилия: {surname}, Возраст: {age} лет."
+print("Реализация через f-string:")
+print(f_string)
