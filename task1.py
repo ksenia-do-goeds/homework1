38da6bacaab26d7c538ce861b22d36244fe55a5f
ksenia-do-goeds <k5ushkaa@yandex.ru>
2025-03-25 13:44:45 +0300

    task1.py

---
+++ task1.py
@@ -0,0 +1,11 @@
+name = input('Ваше имя: ')
+surname = input('Ваша фамилия: ')
+age = input('Ваш возраст: ')
+
+# Реализация через format
+formatted_string = "Ваше имя: {}, Фамилия: {}, Возраст: {} лет.".format(name, surname, age)
+print(formatteg_string)
+
+# Реализация через f-string
+f-string = f"Ваше имя: {name}, Фамилия: {surname}, Возраст: {age} лет."
+print(f-string)
