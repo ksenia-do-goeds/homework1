495a1d8e59c0d765741e66fb642a32fcc2ad6532
ksenia-do-goeds <k5ushkaa@yandex.ru>
2025-03-25 13:56:37 +0300

    task2.py

---
+++ task2.py
@@ -0,0 +1,10 @@
+num = input("Введите число: ")
+
+if num.isdigit()
+  num = int(num)
+  if num%2 == 0:
+    print(f"Число {num} является четным.")
+  else:
+    print(f"Число {num} не является четным.")
+else:
+  print("Ошибка: введено не число.")
